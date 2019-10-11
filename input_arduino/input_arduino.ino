a08aced7d606f939e7136ce38a25b39b02705a4c
hexidian <leokastenberg@gmail.com>
2019-10-11 13:08:02 -0400

    nm

--- input_arduino/input_arduino.ino
+++ input_arduino/input_arduino.ino
@@ -33,8 +33,8 @@ void loop() {
       }
     } else {
       bool this_bit = 1 ? digitalRead(data_pin) : 0;
-      Serial.print("this bit is ");
-      Serial.println(this_bit);
+      //Serial.print("this bit is ");
+      //Serial.println(this_bit);
       message = message >> 1;
       message += this_bit * 0b10000000;
       bits++;
